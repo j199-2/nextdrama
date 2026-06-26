7aaaf885393e34f060bffc89c5abd24c181048c5
j199-2 <joancuevas0818@gmail.com>
2026-06-27 00:06:02 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import Script from 'next/script'
+
 export const metadata = {
   title: 'NextGen DramaFinder',
   description: 'Ecosistema de Creadores',
@@ -7,8 +9,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
-        {/* Esto carga todo el motor de diseño de Tailwind de golpe */}
-        <script src="https://cdn.tailwindcss.com"></script>
+        {/* Cargador oficial y seguro para Next.js */}
+        <Script 
+          src="https://cdn.tailwindcss.com" 
+          strategy="beforeInteractive"
+        />
       </head>
       <body style={{ margin: 0, padding: 0, backgroundColor: '#050608' }}>
         {children}
